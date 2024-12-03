e4499ec4fdd148b96f0a4b249e03ebad01c6b09d
Vu <vuanhdora@gmail.com>
2024-12-03 21:34:06 +0700

    "Update"

---
+++ src/myLinkedList/Node.java
@@ -0,0 +1,15 @@
+package myLinkedList;
+
+public class Node {
+    private int data;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+}
---
+++ src/myLinkedList/myLinkedList.java
@@ -0,0 +1,11 @@
+package myLinkedList;
+
+public class myLinkedList {
+    private Node head;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+}
