25d17de5eea3c0a80798a7033122df3cce3c6af7
Vu <vuanhdora@gmail.com>
2024-12-04 00:22:52 +0700

    "Last"

--- src/myLinkedList/myLinkedList.java
+++ src/myLinkedList/myLinkedList.java
@@ -3,15 +3,47 @@ package myLinkedList;
 public class myLinkedList {
     private Node head;
 
-    public void addFirst(int data){
+    public void addFirst(int data) {
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
     }
 
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+
+    }
+
+    public void add(int data, int index){
+        Node newNode = new Node(data);
+        if ( index == 0){
+            addFirst(data);
+            return;
+        }
+        else{
+            Node temp = head;
+            for ( int i = 0; i < index - 1; i++){
+                temp = temp.next;
+            }
+            Node nextNode = temp.next;
+            temp.next = newNode;
+            newNode.next = nextNode;
+        }
+    }
+
     public static void main(String[] args) {
         myLinkedList linkedList = new myLinkedList();
-        linkedList.addFirst(1);
-        linkedList.addFirst(2);
+        //linkedList.addFirst(1);
+        //linkedList.addFirst(2);
+        linkedList.addLast(12);
     }
 }
